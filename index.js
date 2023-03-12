d0d816330607adf9473a25264a5ab985c3fcf017
sidmadke <siddhant.madke@gmail.com>
2023-03-12 17:50:50 +0530

    Formated and added new products

---
+++ index.js
@@ -0,0 +1,27 @@
+// get search bar element
+const searchInput = document.getElementById("searchInput");
+
+// store name elements in array-like object
+const namesFromDOM = document.getElementsByClassName("product");
+
+// listen for user events
+searchInput.addEventListener("keyup", (event) => {
+    const { value } = event.target;
+    
+    // get user search input converted to lowercase
+    const searchQuery = value.toLowerCase();
+    
+    for (const nameElement of namesFromDOM) {
+        // store name text and convert to lowercase
+        let name = nameElement.textContent.toLowerCase();
+        
+        // compare current name to search input
+        if (name.includes(searchQuery)) {
+            // found name matching search, display it
+            nameElement.style.display = "block";
+        } else {
+            // no match, don't display name
+            nameElement.style.display = "none";
+        }
+    }
+});
